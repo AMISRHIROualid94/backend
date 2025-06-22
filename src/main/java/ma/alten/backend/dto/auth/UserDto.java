2897e1cdfa2b2f60e70763d628c463fa46371ee4
amisrhir oualid <amisrhiroualid94@gmail.com>
2025-06-23 00:24:36 +0100

    add email validation pattern

--- src/main/java/ma/alten/backend/dto/auth/UserDto.java
+++ src/main/java/ma/alten/backend/dto/auth/UserDto.java
@@ -1,9 +1,13 @@
 package ma.alten.backend.dto.auth;
 
+import jakarta.validation.constraints.Pattern;
+import ma.alten.backend.constantes.Validation;
+
 public record UserDto(
         Long id,
         String username,
         String firstname,
+        @Pattern(regexp = Validation.EMAIL_PATTERN,message = Validation.INVALID_EMAIL_PATTERN)
         String email,
         String password
 ) {
